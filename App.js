dd13b5366e4d35014be39475c84e63331f4d2a5b
punIN10ded <imnotfreenow@gmail.com>
2022-03-19 13:51:04 +0530

    helo

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View } from 'react-native';
-import { useFonts, Montserrat_900Black } from '@expo-google-fonts/montserrat';
-import Login from './src/components/Login';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { useFonts, Montserrat_900Black } from "@expo-google-fonts/montserrat";
+import Login from "./src/components/Login";
 
 export default function App() {
+	let [fontsLoaded] = useFonts({
+		Montserrat_900Black,
+	});
 
-  let [fontsLoaded] = useFonts({
-    Montserrat_900Black,
-  });
-  
-  return (
-    <View style={styles.container}>
-      <Login/>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Login />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+	},
 });
